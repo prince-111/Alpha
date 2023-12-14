d282d10a9958a5d6e8bc4a9315095988cc66dafb
Prince Kumar <princesd991@gmail.com>
2023-12-14 10:24:28 +0530

    ❄️ Build and Setup Booking app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+   <Navbar/>
+   <Header/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { DatePicker } from "antd";
+
+const Header = () => {
+
+    const style = {
+        border: `1px solid`,
+        borderRadius: '100%',
+      };
+
+    const cellRender = React.useCallback((current: number | Dayjs, info: CellRenderInfo<Dayjs>) => {
+        if (info.type !== 'date') {
+          return info.originNode;
+        }
+        if (typeof current === 'number') {
+          return <div className="ant-picker-cell-inner">{current}</div>;
+        }
+        return (
+          <div className="ant-picker-cell-inner" style={current.date() === 1 ? style : {}}>
+            {current.date()}
+          </div>
+        );
+      }, []);
+
+    const [destination, setDestination] = useState("");
+  const [openDate, setOpenDate] = useState(false);
+  const [date, setDate] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [options, setOptions] = useState({
+    adult: 1,
+    children: 0,
+    room: 1,
+  });
+
+
+
+  return (
+    <>
+      <div className="m-8">
+        <div class="max-w-lg p-6 mb-5 bg-white border border-gray-200 rounded-lg shadow dark:white dark:white">
+          <form className="mb-5">
+            <label
+              for="default-search"
+              class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div class="relative">
+              <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Your Destination..."
+                required
+              />
+              <button
+                type="submit"
+                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+
+          <div className="pt-4, text-center">
+          <DatePicker.RangePicker cellRender={cellRender} size="large" />
+          </div>
+
+          <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+
+           <div>
+           </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
