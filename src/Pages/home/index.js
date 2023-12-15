cbf15fdb1bb0ddc16111f4b5e250032d040fa7d7
Prince Kumar <princesd991@gmail.com>
2023-12-15 18:10:43 +0530

    🏡 Add The Routing

---
+++ src/Pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+import Header from '../../Components/Header'
+import SliderImages from '../../Components/SliderImages'
+
+const Home = () => {
+  return (
+    <div>
+       <Navbar/>
+       <Header/>
+       <SliderImages/>
+    </div>
+  )
+}
+
+export default Home;
