ecbe5a64a4fbb6b88afdc1d5b1bbfe4a29cdde85
Prince Kumar <princesd991@gmail.com>
2023-12-16 14:33:00 +0530

    🦾 add Hopital Profile

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter,Routes, Route, } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/home";
 import Hotels from "./Components/Hotels";
+import HotelsProfile from "./Components/Hotels/HotelsProfile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/hotels" element={<Hotels/>}/>
+      <Route path="/hotels/:id" element={<HotelsProfile/>}/>
     </Routes>
     </BrowserRouter>
 
